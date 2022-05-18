0b314c37dda7e76821cb812e0bde1c9d2e12ff97
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-17 20:26:27 -0400

    Create admin_constants.php

---
+++ admin/partials/admin_constants.php
@@ -0,0 +1,14 @@
+<?php
+
+//Start Session
+    ob_start();
+    session_start();
+
+        // Connect to database
+        $conn = new mysqli("lucas-lane.c2nbqnphldvm.us-east-1.rds.amazonaws.com","admin","LucasLane2022!","lucas_lane");
+          
+          if($conn->connect_error){
+            die("Connection Failed!".$conn->connect_error);
+          }
+
+?>
