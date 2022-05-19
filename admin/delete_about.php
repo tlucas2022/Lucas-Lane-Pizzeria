4b002f5af6462329d13dbea9887e80cf67500aad
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 03:45:35 -0400

    Update delete_about.php

--- admin/delete_about.php
+++ admin/delete_about.php
@@ -1,38 +1,34 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
 
-    //echo "Delete Page";
-
-        //Get the Value and Delete
-        //echo "Get Value and Delete";
-        $id = $_GET['id'];
-
-
-        //Delete Data from Database
-        //SQL Query to Delete Data from Database
-        $sql = "DELETE FROM ll_about WHERE id=$id";
-
-        //Execute the Query
-        $res = mysqli_query($conn, $sql);
-
-        //Check whether the data is delete from database or not
-        if($res==true)
-        {
-            //SEt Success MEssage and REdirect
-            $_SESSION['delete'] = "<div class='success'>Content Deleted Successfully.</div>";
-            //Redirect to Manage Category
-            header('location:admin/Manage_About.php');
-        }
-        else
-        {
-            //SEt Fail MEssage and Redirecs
-            $_SESSION['delete'] = "<div class='error'>Failed to Delete Content.</div>";
-            //Redirect to Manage Category
-            header('location:admin/Manage_About.php');
-        }
-
- 
-
+<?php
   
+//Include Constants File
+include('partials/admin_constants.php');
+
+//Get the id and Delete 
+$id = $_GET['id'];
+
+//Delete Data from Database
+$sql = "DELETE FROM ll_about WHERE id=$id";
+
+//Execute the Query
+$res = mysqli_query($conn, $sql);
+
+//Check if the data has been deleted from database
+if($res==true)     
+{     
+    //Set success message
+    $_SESSION['delete'] = "<div class='success'>Content Deleted Successfully.</div>";
+            
+    //Redirect to Manage About
+    header('location:Manage_About.php');     
+}
+else
+{      
+    //Set Fail Message 
+     $_SESSION['delete'] = "<div class='error'>Failed to Delete Content.</div>";
+ 
+    //Redirect to Manage About    
+    header('location:Manage_About.php');       
+}
+
 ?>
