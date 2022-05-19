2b634361ef9817e21de94e7a7ed78cb4c467c707
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 02:46:33 -0400

    Create navbar.php

---
+++ partial/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+       <a class="nav-link" href="index.php">HOME</a>
+    </li>
+   
+    <li class="nav-item">
+      <a class="nav-link" href="Menu.php">MENU</a>
+    </li>
+    
+    <li class="nav-item">
+      <a class="nav-link" href="About.php">ABOUT</a>
+    </li>
+    
+    <li class="nav-item">
+      <a class="nav-link" href="Online Ordering.php">ONLINE ORDERING</a>
+    </li>
+    
+    <li class="nav-item">
+      <a class="nav-link" href="Contact.php">CONTACT US</a>
+    </li>
+    
+  </ul>
+</nav>
+    
+<p>  </p>
