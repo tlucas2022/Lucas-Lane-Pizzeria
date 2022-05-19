62ae9e3e920bf9f7fed3c143a5f6de901c78675d
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 03:49:09 -0400

    Update delete_category.php

--- admin/delete_category.php
+++ admin/delete_category.php
@@ -1,37 +1,33 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
+<?php
+    
+//Include Constants File
+include('partials/admin_constants.php');
 
-    //echo "Delete Page";
+//Get the id and Delete
+$id = $_GET['id'];
+        
+//Delete Data from Database
+$sql = "DELETE FROM ll_category WHERE id=$id";
 
-        //Get the Value and Delete
-        //echo "Get Value and Delete";
-        $id = $_GET['id'];
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-        //Delete Data from Database
-        //SQL Query to Delete Data from Database
-        $sql = "DELETE FROM ll_category WHERE id=$id";
-
-        //Execute the Query
-        $res = mysqli_query($conn, $sql);
-
-        //Check whether the data is delete from database or not
-        if($res==true)
-        {
-            //SEt Success MEssage and REdirect
-            $_SESSION['delete'] = "<div class='success'>Category Deleted Successfully.</div>";
-            //Redirect to Manage Category
-            header('location:admin/manage_category.php');
-        }
-        else
-        {
-            //SEt Fail MEssage and Redirecs
-            $_SESSION['delete'] = "<div class='error'>Failed to Delete Category.</div>";
-            //Redirect to Manage Category
-            header('location:admin/manage_category.php');
-        }
-
- 
-
+//Check if the data has been deleted from database 
+if($res==true)
+{
+    //Set success message 
+    $_SESSION['delete'] = "<div class='success'>Category Deleted Successfully.</div>$
+    
+    //Redirect to Manage Category    
+    header('location:manage_category.php');
+ } 
+ else  
+ {
+    //Set Fail Message 
+    $_SESSION['delete'] = "<div class='error'>Failed to Delete Category.</div>";
   
+    //Redirect to Manage Category
+    header('location:manage_category.php');
+ } 
+   
 ?>
