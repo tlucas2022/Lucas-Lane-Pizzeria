6fb25b371d1d0517693f418cd0150d6b509a3218
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 03:41:50 -0400

    Update delete_admin.php

--- admin/delete_admin.php
+++ admin/delete_admin.php
@@ -1,35 +1,31 @@
-<?php 
- include('partials/admin_constants.php');
-    //Include constants.php file here
+<?php
+include('partials/admin_constants.php');
+//Include constants.php file here
 
-    // 1. get the ID of Admin to be deleted
-    $id = $_GET['id'];
+// 1. get the ID of Admin to be deleted
+$id = $_GET['id'];
 
-    //2. Create SQL Query to Delete Admin
-    $sql = "DELETE FROM ll_admin WHERE id=$id";
+//2. Create SQL Query to Delete Admin
+$sql = "DELETE FROM ll_admin WHERE id=$id";
 
-    //Execute the Query
-    $res = mysqli_query($conn, $sql);
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-    // Check whether the query executed successfully or not
-    if($res==true)
-    {
-        //Query Executed Successully and Admin Deleted
-        //echo "Admin Deleted";
-        //Create SEssion Variable to Display Message
-        $_SESSION['delete'] = "<div class='success'>Admin Deleted Successfully.</div>";
-        //Redirect to Manage Admin Page
-        header('location:admin/Manage_Admins.php');
-    }
-    else
-    {
-        //Failed to Delete Admin
-        //echo "Failed to Delete Admin";
+// Check whether the query executed successfully or not
+if($res==true)
+{
+ //Query Executed Successully and Admin Deleted
+ $_SESSION['delete'] = "<div class='success'>Admin Deleted Successfully.</div>";
 
-        $_SESSION['delete'] = "<div class='error'>Failed to Delete Admin. Try Again Later.</div>";
-        header('location:admin/Manage_Admins.php');
-    }
+ //Redirect to Manage Admin Page
+  header('location:Manage_Admins.php');
+}
+else
+{
+ //Failed to Delete Admin
+ $_SESSION['delete'] = "<div class='error'>Failed to Delete Admin. Try Again Later.</div>";
 
-    //3. Redirect to Manage Admin page with message (success/error)
-
+ header('location:Manage_Admins.php');
+}
+   
 ?>
