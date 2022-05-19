8fd6800e560cca41c7dfea4fe19d513b5b6fbe2a
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 04:06:20 -0400

    Update delete_item.php

--- admin/delete_item.php
+++ admin/delete_item.php
@@ -1,38 +1,32 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
+<?php
+    
+//Include Constants File
+include('partials/admin_constants.php');
 
-    //echo "Delete Page";
+//Get the Value and Delete
+$id = $_GET['id'];
 
-        //Get the Value and Delete
-        //echo "Get Value and Delete";
-        $id = $_GET['id'];
+//Delete Data from Database
+$sql = "DELETE FROM ll_food WHERE id=$id";
 
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-        //Delete Data from Database
-        //SQL Query to Delete Data from Database
-        $sql = "DELETE FROM ll_food WHERE id=$id";
+if($res==true)      
+{          
+    //Set Success Message 
+    $_SESSION['delete'] = "<div class='success'>Food Deleted Successfully.</div>";
+            
+    //Redirect to Manage Items   
+    header('location:manage_items.php');      
+}       
+else 
+{
+    //Set Fail Message 
+    $_SESSION['delete'] = "<div class='error'>Failed to Delete Food.</div>";
+            
+    //Redirect to Manage Items
+    header('location:manage_items.php');        
+}
 
-        //Execute the Query
-        $res = mysqli_query($conn, $sql);
-
-        //Check whether the data is delete from database or not
-        if($res==true)
-        {
-            //SEt Success MEssage and REdirect
-            $_SESSION['delete'] = "<div class='success'>Food Deleted Successfully.</div>";
-            //Redirect to Manage Food
-            header('location:admin/manage_items.php');
-        }
-        else
-        {
-            //SEt Fail MEssage and Redirecs
-            $_SESSION['delete'] = "<div class='error'>Failed to Delete Food.</div>";
-            //Redirect to Manage Food
-            header('location:admin/manage_items.php');
-        }
-
- 
-
-  
 ?>
