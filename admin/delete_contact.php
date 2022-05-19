3521b16c44025857f702f4ba169134f8bcd4b3f5
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 03:56:13 -0400

    Update delete_contact.php

--- admin/delete_contact.php
+++ admin/delete_contact.php
@@ -1,37 +1,33 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
+<?php
 
-    //echo "Delete Page";
+//Include Constants File
+include('partials/admin_constants.php');
 
-        //Get the Value and Delete
-        //echo "Get Value and Delete";
-        $id = $_GET['id'];
+//Get the Value and Delete
+$id = $_GET['id'];
 
-        //Delete Data from Database
-        //SQL Query to Delete Data from Database
-        $sql = "DELETE FROM ll_contact WHERE id=$id";
+//Delete Data from Database
+$sql = "DELETE FROM ll_contact WHERE id=$id";
 
-        //Execute the Query
-        $res = mysqli_query($conn, $sql);
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-        //Check whether the data is delete from database or not
-        if($res==true)
-        {
-            //SEt Success MEssage and REdirect
-            $_SESSION['delete'] = "<div class='success'>Content Deleted Successfully.</div>";
-            //Redirect to Manage Category
-            header('location:admin/Manage_Contact.php');
-        }
-        else
-        {
-            //SEt Fail MEssage and Redirecs
-            $_SESSION['delete'] = "<div class='error'>Failed to Delete Content.</div>";
-            //Redirect to Manage Category
-            header('location:admin/Manage_Contact.php');
-        }
+//Check if the data has been deleted from database 
+if($res==true)
+{
+    //Set Success Message
+    $_SESSION['delete'] = "<div class='success'>Content Deleted Successfully.</div>";
 
- 
+    //Redirect to Manage Contact    
+    header('location:Manage_Contact.php');       
+}       
+else 
+{   
+    //Set Fail Message
+    $_SESSION['delete'] = "<div class='error'>Failed to Delete Content.</div>";
 
-  
+    //Redirect to Manage Contact
+    header('location:Manage_Contact.php');       
+}
+
 ?>
