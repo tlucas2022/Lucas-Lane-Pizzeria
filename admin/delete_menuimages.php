1eba199bc2fb9cc32cda101fb493e1b0613578fb
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 04:14:50 -0400

    Update delete_menuimages.php

--- admin/delete_menuimages.php
+++ admin/delete_menuimages.php
@@ -1,35 +1,38 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
-
-    $id = $_GET['id'];
-    $filename = $_GET['filename'];
-
+<?php
     
+//Include Constants File
+include('partials/admin_constants.php');
+    
+$id = $_GET['id'];
+$filename = $_GET['filename'];
 
-    $path = "Images/".$filename;
+$path = "../Images/".$filename;
+            
+//REmove Image File from Folder
+$remove = unlink($path);
+    
+//SQL Query to Delete Data from Database
+$sql = "DELETE FROM ll_menu WHERE id=$id";
+    
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-            //REmove Image File from Folder
-    $remove = unlink($path);
-
-    //SQL Query to Delete Data from Database
-    $sql = "DELETE FROM ll_menu WHERE id=$id";
-
-    //Execute the Query
-    $res = mysqli_query($conn, $sql);
-
-    if($res==true)
-        {
-            //Food Deleted
-            $_SESSION['delete'] = "<div class='success'>Menu Image Deleted Successfully.</div>";\
-            header('location:admin/manage_menuimages.php');
-        }
-        else
-        {
-            //Failed to Delete Food
-            $_SESSION['delete'] = "<div class='error'>Menu Image Not Deleted.</div>";\
-            header('location:admin/manage_menuimages.php');
-        }
-
+if($res==true)
+{        
+    //Image Deleted
+    $_SESSION['delete'] = "<div class='success'>Menu Image Deleted Successfully.</div>
+            
+    //Redirect to Manage Menu Images      
+    header('location:manage_menuimages.php');    
+}
         
+ else
+ {
+     //Failed to Delete Food
+     $_SESSION['delete'] = "<div class='error'>Menu Image Not Deleted.</div>";\
+    
+    //Redirect to Manage Menu Images     
+    header('location:manage_menuimages.php');  
+ }
+
 ?>
