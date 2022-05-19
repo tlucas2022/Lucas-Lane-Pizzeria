992606700b32aa174550bbcff74bfeb8731a754c
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 02:56:47 -0400

    Update user_constants.php

--- partial/user_constants.php
+++ partial/user_constants.php
@@ -1,15 +1,12 @@
-<?php include "../inc/dbinfo.inc"; ?> <!-- use connection file -->
-
 <?php
     //Start Session
     ob_start();
     session_start();
 
- /* Connect to MySQL and select the database. */
- $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-
- if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
- $database = mysqli_select_db($conn, DB_DATABASE);
-
+        // Connect to database
+        $conn = new mysqli("lucas-lane.c2nbqnphldvm.us-east-1.rds.amazonaws.com","admin","LucasLane2022!","lucas_lane");
+          
+          if($conn->connect_error){
+            die("Connection Failed!".$conn->connect_error);
+          }
 ?>
