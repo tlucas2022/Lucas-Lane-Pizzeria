0bdc54da9fcb9dc9b3537d395bc0521221cf51e5
tlucas2022 <105578250+tlucas2022@users.noreply.github.com>
2022-05-19 04:19:08 -0400

    Update delete_menupdf.php

--- admin/delete_menupdf.php
+++ admin/delete_menupdf.php
@@ -1,45 +1,51 @@
-<?php 
-    //Include Constants File
-    include('partials/admin_constants.php');
+<?php
 
-    $id = $_GET['id'];
+//Include Constants File
+include('partials/admin_constants.php');
 
-    $sql1= "SELECT pdf FROM ll_menu_pdf WHERE id=$id";
+$id = $_GET['id'];
 
-    //Execute the Query
-    $res1 = mysqli_query($conn, $sql1);
+// Query to find the pdf
+$sql1= "SELECT pdf FROM ll_menu_pdf WHERE id=$id";
 
-    $count = mysqli_num_rows($res1);
+//Execute the Query
+$res1 = mysqli_query($conn, $sql1);
 
-    if($count>0)
-    {
-        $row=mysqli_fetch_array($res1);
-        $filename = $row['pdf'];
-        $path = "html/PDF/".$filename;
+// Count Rows    
+$count = mysqli_num_rows($res1);
 
-            //REmove Image File from Folder
-             $remove = unlink($path);
-    }
-    else {}
+if($count>0)
+{
+    $row=mysqli_fetch_array($res1);
+    $filename = $row['pdf'];
+    $path = "../PDF/".$filename;
 
-    //SQL Query to Delete Data from Database
-    $sql = "DELETE FROM ll_menu_pdf WHERE id=$id";
+    //REmove Image File from Folder
+    $remove = unlink($path);
+}
+else {}
 
-    //Execute the Query
-    $res = mysqli_query($conn, $sql);
+//SQL Query to Delete PDF from Database
+$sql = "DELETE FROM ll_menu_pdf WHERE id=$id";
 
-    if($res==true)
-        {
-            //Food Deleted
-            $_SESSION['delete'] = "<div class='success'>Menu PDF Deleted Successfully.</div>";\
-            header('location:admin/manage_menupdf.php');
-        }
-        else
-        {
-            //Failed to Delete Food
-            $_SESSION['delete'] = "<div class='error'>Menu PDF Not Deleted.</div>";\
-            header('location:admin/manage_menupdf.php');
-        }
+//Execute the Query
+$res = mysqli_query($conn, $sql);
 
-        
+if($res==true)
+{            
+    //PDF Deleted
+     $_SESSION['delete'] = "<div class='success'>Menu PDF Deleted Successfully.</div>";\
+ 
+    //Redirect to Manage Menu PDF  
+    header('location:manage_menupdf.php');  
+}      
+else
+{   
+    //Failed to Delete PDF 
+    $_SESSION['delete'] = "<div class='error'>Menu PDF Not Deleted.</div>";\
+    
+    //Redirect to Manage Menu PDF         
+    header('location:manage_menupdf.php');
+}
+
 ?>
